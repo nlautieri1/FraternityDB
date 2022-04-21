70fa65b6d846c043641f8ae73cb9bb00c5b4ddd8
mdemchuk <101371223+mdemchuk@users.noreply.github.com>
2022-04-21 09:47:53 -0400

    Update admin.php

--- html/admin.php
+++ html/admin.php
@@ -4,7 +4,7 @@
 
 	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous">
 	</script>
-	<link rel="stylesheet" href="admin.css">
+	<link rel="stylesheet" href="css/admin.css">
 
 	<script src="js/main.js"></script>	
 	</head>
