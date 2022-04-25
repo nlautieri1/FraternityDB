3b21a1c6fea8e7d6655ee51f88aeb8f86ca181c5
nlautieri1 <103210839+nlautieri1@users.noreply.github.com>
2022-04-24 21:52:57 -0400

    Update pnm.php

--- html/newFiles/pnm.php
+++ html/newFiles/pnm.php
@@ -15,9 +15,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/home.css">    
-    <?php
-	echo "<title>{$_SESSION['fName']} PNM's</title>";
-    ?>
+<title>Salisbury Fraternity Homepage</title>
     <style>
 	.navbar-red{
 		background:#bf110c;
@@ -26,8 +24,8 @@
   </head>
   <body>
 <nav class="navbar navbar-expand-lg navbar-dark navbar-red">
-  <img src="images/Salisbury_University_logo.png" width="150" height="50" alt="">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <img src="images/Salisbury_University_logo.png" width="150" height="50" alt="">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
@@ -50,13 +48,127 @@
       <li class="nav-item">
         <a class="nav-link" href="dues.php">Dues</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="pnm.php">PNM's</a>
-      </li>
     </ul>
       <a href="logout.php"><button class="btn btn-outline-light my-2 my-sm-0" type="submit">Logout</button></a>
   </div>
 </nav>
+
+
+
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col text-center">
+<?php
+echo "<h1> {$_SESSION['fName']} PNM's</h1>";
+?>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Insert</button>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Delete</button>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Update</button>
+    </div>
+  </div>
+</div>
+
+<form method="post">
+
+                Search by: <select name="attribute">
+                        <option value="firstName">First Name</option>
+                        <option value="lastName">Last Name</option>
+                        <option value="phone">Phone</option>
+                        <option value="email">Email</option>
+                        <option value="grade">Grade</option>
+                        <option value="gpa">GPA</option>
+                </select>
+
+
+                <input id="search" name="pnm_input" type="text" placeholder="Type here">
+                <input id="submit" type="submit" value="Search">
+        </form>
+
+<br>
+
+<table class="table table-striped">
+  <thead>
+    <tr> 
+      <th scope="col">Email</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Grade</th>
+      <th scope="col">GPA</th>
+      <th scope="col">Event Name</th>
+      <th scope="col">Date</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+$connection=mysqli_connect("localhost", "nlautieri1", "3Cavalier3gulls", "FraternityDB") or die("Error connecting to database: ".mysqli_error());
+
+$pnm_input=$_POST['pnm_input'];
+$attribute=$_POST['attribute'];
+
+if ($attribute == 'firstName') {
+        $query="Select * From PNM natural join Rush Where firstName Like '%$pnm_input%' and fName = '{$_SESSION['fName']}'";
+} else if ($attribute == 'lastName') {
+        $query="Select * From PNM natural join Rush Where lastName Like '%$pnm_input%' and fName = '{$_SESSION['fName']}'";
+} else if ($attribute == 'phone') {
+        $query="Select * From PNM natural join Rush Where phone Like '%$pnm_input%' and fName = '{$_SESSION['fName']}'";
+} else if ($attribute == 'email') {
+        $query="Select * From PNM natural join Rush Where email Like '%$pnm_input%' and fName = '{$_SESSION['fName']}'";
+} else if ($attribute == 'gpa') {
+        $query="Select * From PNM natural join Rush Where gpa >= '$pnm_input' and fName = '{$_SESSION['fName']}'";
+} else if ($attribute == 'grade') {
+        $query="Select * From PNM natural join Rush Where grade Like '%$pnm_input%' and fName = '{$_SESSION['fName']}'";
+}
+
+$r=mysqli_query($connection, $query);
+        while ($row=mysqli_fetch_array($r)) {
+                echo "<tr>";
+                echo "<th scope='row'>".$row['email']."</th>";
+                echo "<td>".$row['firstName']."</td>";
+                echo "<td>".$row['lastName']."</td>";
+                echo "<td>".$row['phone']."</td>";
+                echo "<td>".$row['grade']."</td>";
+                echo "<td>".$row['gpa']."</td>";
+                echo "<td>".$row['eventName']."</td>";
+                echo "<td>".$row['date']."</td>";
+                echo "</tr>";
+        }
+mysqli_close($connection);
+?>
+  </tbody>
+</table>
+
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div class="FooterContainer">
 	<footer>Location: Guerrieri Student Union 125 | Phone Number: 410-543-6125</footer>
@@ -70,4 +182,3 @@
 	
 	</body>
 </html>
-
