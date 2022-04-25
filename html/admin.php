1f1b361a3049b1a2310f4ccb8e77f0e612aff5ec
mdemchuk <101371223+mdemchuk@users.noreply.github.com>
2022-04-25 01:46:08 -0400

    Update admin.php

--- html/admin.php
+++ html/admin.php
@@ -1,48 +1,74 @@
-<html>
-	<head>
-	<title>SU Fraternity Greek Life</title>
+<?php
+	session_start();
+	if(!isset($_SESSION['fName'])){
+		header("location: login.php");
+	}
+?>
 
-	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous">
-	</script>
-	<link rel="stylesheet" href="css/admin.css">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<script src="js/main.js"></script>	
-	</head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/home.css">    
+    <?php
+	echo "<title>{$_SESSION['fName']} Home</title>";
+    ?>
 
-	<body>
+    <style>
+	.navbar-red{
+		background:#bf110c;
+	}
+    </style>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-lg navbar-dark navbar-red">
+  <img src="images/Salisbury_University_logo.png" width="150" height="50" alt="">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="admin.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="members.php">Members</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="alumni.php">Alumni</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="communityService.php">Community Service</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="attendance.php">Attendance</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="dues.php">Dues</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pnm.php">PNM's</a>
+      </li>
+    </ul>
+      <a href="logout.php"><button class="btn btn-outline-light my-2 my-sm-0" type="submit">Logout</button></a>
+  </div>
+</nav>
+
+<div class="FooterContainer">
+	<footer>Location: Guerrieri Student Union 125 | Phone Number: 410-543-6125</footer>
+</div>
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	
-	<div class="TopNav">
-		<a href="#Home" class="Selected"> Salisbury University Fraternity Greek Life</a>
-		<a href="#Members">Members</a>
-		<a href="#Alumni">Alumni</a>
-		<a href="#Community Service">Community Service</a>
-		<a href="#Attendence">Attendence</a>
-		<a href="#Dues">Dues</a>
-		<a href="" class="LogoutButton">Logout</a>
-	</div>
-	
-	<div class="NavSpace">
-	</div>
-	
-	<div class="InfoContainer">
-	
-	<div id="Toprow">
-		<span class="indented">This section will contain a description about the logged-in fraternity's members</span>
-		<span>This section will contain a description about the logged-in fraternity's alumni</span>
-		<span>This section will contain a description about the logged-in fraternity's community service</span>
-	</div>
-	
-	<div id="Bottomrow">
-		<span class="indented">This section will contain a description about the logged-in fraternity's attendence</span>
-		<span>This section will contain a description about the logged-in fraternity's dues</span>
-		<span>This section will contain a description about the logged-in fraternity's Potential New Members</span>
-	</div>
-	
-	</div>
-	
-	<div class="FooterContainer">
-	<footer>Global Admin Contact Information Here</footer>
-	</div>
 	</body>
 </html>
 
