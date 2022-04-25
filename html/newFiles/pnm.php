8736db7221f85ad33e2c13fda244744017ed7ff3
mdemchuk <101371223+mdemchuk@users.noreply.github.com>
2022-04-24 20:48:00 -0400

    Create pnm.php

---
+++ html/newFiles/pnm.php
@@ -0,0 +1,73 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['fName'])){
+		header("location: login.php");
+	}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/home.css">    
+    <?php
+	echo "<title>{$_SESSION['fName']} PNM's</title>";
+    ?>
+    <style>
+	.navbar-red{
+		background:#bf110c;
+	}
+    </style>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-lg navbar-dark navbar-red">
+  <img src="images/Salisbury_University_logo.png" width="150" height="50" alt="">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="admin.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="members.php">Members</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="alumni.php">Alumni</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="communityService.php">Community Service</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="attendance.php">Attendance</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="dues.php">Dues</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pnm.php">PNM's</a>
+      </li>
+    </ul>
+      <a href="logout.php"><button class="btn btn-outline-light my-2 my-sm-0" type="submit">Logout</button></a>
+  </div>
+</nav>
+
+<div class="FooterContainer">
+	<footer>Location: Guerrieri Student Union 125 | Phone Number: 410-543-6125</footer>
+</div>
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	
+	</body>
+</html>
+
