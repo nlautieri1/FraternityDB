32fd0d652f0ddaf4b12b0cb82bf0a0085051bc83
imoki1 <92948511+imoki1@users.noreply.github.com>
2022-05-07 15:58:42 -0400

    Update login.php

--- Final Product/login.php
+++ Final Product/login.php
@@ -1,35 +1,67 @@
 <?php
     session_start();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Login Page</title>
-	<link href="css/login.css" rel="stylesheet">
-</head>
-<body>
-	<center>
-		<h1>Fraternity Login</h1>
-	</center>
-	<form action="login.php" method="post">
-		<div class="container">
-			<label>Username:</label><br>
-			<input maxlength="20" name="username" placeholder="Enter Username" required="" type="text"><br>
-			<label>Password:</label><br>
-			<input maxlength="20" name="password" placeholder="Enter Password" required="" type="password"> <button name="login_btn" type="submit" value="login">Login</button> <?php
-			        
-			        if($connection=@mysqli_connect('localhost', 'mdemchuk1', 'Coors478Ultra', 'FraternityDB')){}
-			        $username = $_POST['username'];
-			        $password = $_POST['password'];
+<!doctype html>
+<html lang="en">
+	<head>
+   		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-			        $result = mysqli_query($connection, "select * from Account where username='$username' and password='$password'");
-			        if(mysqli_num_rows($result) != 0){
-			            $row = mysqli_fetch_array($result);
-			            $_SESSION['fName'] = $row[0];
-			            header("Location: admin.php");
-			        }
-			    ?>
-		</div>
-	</form>
+   		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<style>
+			body
+			{
+				background-color:lightgrey;
+			}
+		</style>	
+
+		<title>Fraterntiy Login</title>
+	
+	</head>
+	<body>
+
+		<h1 class="mt-3 mb-4 text-center">Fraternity Login</h1>
+		
+		<form action="login.php" method="post">
+			<div class="fborder border border-secondary bg-light py-5 w-50 d-block mx-auto" style="border-style: groove;">
+				<img alt="" class="d-block h-25 w-25 mx-auto" src="images/Salisbury_University_colored_logo.png"><br>
+				<div class="form-group">
+					<label class="d-block text-center">Username:</label>
+					<input class="form-control w-50 mx-auto" maxlength="20" name="username" placeholder="Enter Username" required="" type="text"><br>
+					<label class="d-block text-center"> Password:</label>
+					<input class="form-control w-50 mx-auto" maxlength="20" name="password" placeholder="Enter Password" required="" type="password"> 
+				</div>
+				
+				<div class="form-group">
+					<!-- Submit button -->
+					<button type="button" class="btn btn-primary btn-block mb-4 w-50 mx-auto">Sign in</button>
+					<!-- Register button -->
+					<div class="text-center">
+						<p>Is your fraternity not listed?<a href="#!">Request an Account</a></p>
+					</div>
+				</div>
+			</div>
+				<?php
+					if($connection=@mysqli_connect('localhost', 'mdemchuk1', 'Coors478Ultra', 'FraternityDB')){}
+			       	 	$username = $_POST['username'];
+				        $password = $_POST['password'];
+	
+				        $result = mysqli_query($connection, "select * from Account where username='$username' and password='$password'");
+				            if(mysqli_num_rows($result) != 0){
+				            $row = mysqli_fetch_array($result);
+				            $_SESSION['fName'] = $row[0];
+				            header("Location: admin.php");
+				        }
+				?>
+		</form>
+		
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
